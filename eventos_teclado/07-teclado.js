49654330d1ec5eb812e03c04a996aa20264df0da
Agustín Comolli <agustincomolli@gmail.com>
2022-07-16 13:56:35 -0300

    Modificación de variable teclas y reordenamiento de código.

--- eventos_teclado/07-teclado.js
+++ eventos_teclado/07-teclado.js
@@ -1,8 +1,30 @@
+function dibujar_linea(lienzo, color, x_inicio, y_inicio, x_fin, y_fin)
+/*
+    DESCRIPCION: Dibuja una línea.
+    PARAMETROS:
+        lienzo = objeto - es la superficie donde se va a dibujar.
+        color = string - color de la línea (red,  green, blue, etc)
+        x_inicio = int - posición inicial de la línea en el eje X
+        y_inicio = int - posición inicial de la línea en el eje Y
+        x_fin = int - posición final de la línea en el eje X
+        y_fin = int - posicón final de la línea en el eje Y
+*/
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3
+    lienzo.moveTo(x_inicio, y_inicio);
+    lienzo.lineTo(x_fin, y_fin);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+
 function dibujar(detalles_evento)
 {
     /* 
         DESCRIPCION:
-        PARAMETROS: detelles_evento: tiene todos los datos del evento ocurrido, en
+        PARAMETROS: detelles_evento = tiene todos los datos del evento ocurrido, en
         este caso el más importante es el keyCode para saber la tecla oprimida.
     */
 
@@ -10,25 +32,31 @@ function dibujar(detalles_evento)
     {
         IZQUIERDA: 37,  // Flecha izquierda
         ARRIBA: 38,     // Flecha arriba
-        ABAJO: 39,      // Flecha derecha
-        DERECHA: 40     // Flecha abajo
+        DERECHA: 39,    // Flecha abajo
+        ABAJO: 40       // Flecha derecha
     };
+    var color = "darkcyan";
+    var movimiento = 10;
 
     if (detalles_evento.keyCode == teclas.IZQUIERDA)
     {
-        console.log("Tecla presionada: " + detalles_evento.key);
+        dibujar_linea(superficie, color, x, y, x - movimiento, y);
+        x -= movimiento;
     }
     else if (detalles_evento.keyCode == teclas.ARRIBA)
     {
-        console.log("Tecla presionada: " + detalles_evento.key);
+        dibujar_linea(superficie, color, x, y, x, y - movimiento);
+        y -= movimiento;
     }
     else if (detalles_evento.keyCode == teclas.ABAJO)
     {
-        console.log("Tecla presionada: " + detalles_evento.key);
+        dibujar_linea(superficie, color, x, y, x, y + movimiento);
+        y += movimiento;
     }
     else if (detalles_evento.keyCode == teclas.DERECHA)
     {
-        console.log("Tecla presionada: " + detalles_evento.key);
+        dibujar_linea(superficie, color, x, y, x + movimiento, y);
+        x += movimiento;
     }
     else
     {
@@ -38,4 +66,13 @@ function dibujar(detalles_evento)
 }
 
 
+// Agregar EventListener
 document.addEventListener("keyup", dibujar);
+// Obtener elemento html através de si Id.
+var dibujo_canvas = document.getElementById("area_dibujo");
+// Obtener el área de dibujo.
+var superficie = dibujo_canvas.getContext("2d");
+var x = 150;
+var y = 150;
+
+dibujar_linea(superficie, "blue", 149, 149, 151, 151);
