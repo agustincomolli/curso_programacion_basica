1314a049d8939c67651b1a291778820d004c8164
Agustín Comolli <agustincomolli@gmail.com>
2022-07-22 13:29:49 -0300

    Agregada estructura básica al desafío.

--- villa_platzi/09-desafio.js
+++ villa_platzi/09-desafio.js
@@ -22,45 +22,10 @@ function dibujar()
    {
         superficie.drawImage(fondo.imagen, 0, 0);
    }
-   if (vaca.esta_cargado)
+   if (granjero.esta_cargado)
    {
-        for (var i= 0; i < vaca.cant_max; i++)
-        {
-            /* 
-                Para organizar los animales en una grilla
-                ancho canvas = 500
-                ancho animal = 80
-                posicion = parseInt(500 / 80) --> 6
-            */
-            var x = num_aleatorio(0, 5); // le resto 1 para que entren
-            var y = num_aleatorio(0, 5); // todos en el canvas.
-            x = x * 80; // ancho del animal.
-            y = y * 80; // alto del animal.
-            superficie.drawImage(vaca.imagen, x, y);
-        }
+        superficie.drawImage(granjero.imagen, 210, 210);
    }
-   if (pollo.esta_cargado)
-   {
-    for (var i= 0; i < pollo.cant_max; i++)
-    {
-        var x = num_aleatorio(0, 5);
-        var y = num_aleatorio(0, 5);
-        x = x * 80;
-        y = y * 80;
-        superficie.drawImage(pollo.imagen, x, y);
-    }
-}
-   if (cerdo.esta_cargado)
-   {
-    for (var i= 0; i < cerdo.cant_max; i++)
-    {
-        var x = num_aleatorio(0, 5);
-        var y = num_aleatorio(0, 5);
-        x = x * 80;
-        y = y * 80;
-        superficie.drawImage(cerdo.imagen, x, y);
-    }
-}
 }
 
 
@@ -75,32 +40,12 @@ function cargar_fondo()
 }
 
 
-function cargar_cerdos()
+function cargar_granjero()
 {
     /*     
     DESCRIPCION: Indica que la imágen está lista para ser dibujada.
     */
-    cerdo.esta_cargado = true;
-    dibujar();
-}
-
-
-function cargar_pollos()
-{
-    /*     
-    DESCRIPCION: Indica que la imágen está lista para ser dibujada.
-    */
-    pollo.esta_cargado = true;
-    dibujar();
-}
-
-
-function cargar_vacas()
-{
-    /*     
-    DESCRIPCION: Indica que la imágen está lista para ser dibujada.
-    */
-    vaca.esta_cargado = true;
+    granjero.esta_cargado = true;
     dibujar();
 }
 
@@ -113,38 +58,20 @@ var fondo =
     url: "tile.png",
     esta_cargado: false
 };
-var vaca = 
+var granjero = 
 {
-    url: "vaca.png",
+    url: "granjero.png",
     esta_cargado: false,
-    cant_max: 10
-};
-var pollo = 
-{
-    url: "pollo.png",
-    esta_cargado: false,
-    cant_max: 10
-};
-var cerdo = 
-{
-    url: "cerdo.png",
-    esta_cargado: false,
-    cant_max: 10
 };
 
 // Agregar objeto Image a las variables.
 fondo.imagen = new Image(); 
-vaca.imagen = new Image();
-cerdo.imagen = new Image();
-pollo.imagen = new Image();
+granjero.imagen = new Image();
 
 fondo.imagen.src = fondo.url;
-vaca.imagen.src = vaca.url;
-cerdo.imagen.src = cerdo.url;
-pollo.imagen.src = pollo.url
+granjero.imagen.src = granjero.url;
 
 // Agregar manejadores de eventos.
 fondo.imagen.addEventListener("load", cargar_fondo);
-cerdo.imagen.addEventListener("load", cargar_cerdos);
-pollo.imagen.addEventListener("load", cargar_pollos);
-vaca.imagen.addEventListener("load", cargar_vacas);
+granjero.imagen.addEventListener("load", cargar_granjero);
+document.addEventListener("keydown", dibujar)
