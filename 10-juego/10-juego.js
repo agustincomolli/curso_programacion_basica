de526e7b3fa6bf92054b5400915885f19969ad06
Agustín Comolli <agustincomolli@gmail.com>
2022-07-25 22:53:52 -0300

    Agregada la clase Guerrero al juego.

---
+++ 10-juego/10-juego.js
@@ -0,0 +1,11 @@
+class Guerrero
+{
+    constructor(nombre)
+    {
+        this.nombre = nombre;
+    }
+}
+
+var arquero = new Guerrero("Legolas");
+var caballero = new Guerrero("Aragorn");
+var mago = new Guerrero("Gandalf");
